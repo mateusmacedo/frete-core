76cfdae96cc656e89ae6a429211fcd787e03824d
Caio Vinicius Alves Ferreira <caio.ferreira@fretebras.com.br>
2023-05-08 18:51:14 -0300

    feat: added functions execute factories and buildEnum

--- src/Domain/AbstractFactory.php
+++ src/Domain/AbstractFactory.php
@@ -11,4 +11,31 @@ abstract class AbstractFactory
     abstract public function create(string $target, mixed $data = null): mixed;
 
     abstract protected function reset(): void;
+     public static function buildEnum($className, $data): mixed
+    {
+        $instance = $className::tryfrom($data);
+        if ($instance instanceof $className) {
+            return $instance;
+        }
+        return null;
+    }
+
+    public function executeFactories($dataFactory, $data)
+    {
+        foreach ($data as $key => $value) {
+            if (array_key_exists($key, $dataFactory)) {
+                $factoryKey = $dataFactory[$key];
+                if (array_key_exists('listName', $factoryKey)) {
+                    $array = [];
+                    foreach ($value as $val) {
+                        $array[] = $factoryKey['factory']->create($val);
+                    }
+                    $this->item->{$factoryKey['set']}(new $factoryKey['listName']($array));
+                } else {
+                    $factory = $factoryKey['factory']->create($value);
+                    $this->item->{$factoryKey['set']}($factory);
+                }
+            }
+        }
+    }
 }
