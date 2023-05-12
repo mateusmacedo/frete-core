601732fc7db677dd9e4c9e3ac05bf597faa09fca
Igor de Paula <igor.paula@fretebras.com.br>
2023-05-12 09:57:20 -0300

    feat: add Infrastructure Error class

---
+++ src/Infrastructure/Errors/InfrastructureError.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Frete\Core\Application\Errors;
+
+use Frete\Core\Shared\Result;
+
+class InfrastructureError extends Result
+{
+    public function __construct($errors)
+    {
+        parent::__construct(false, null, $errors);
+    }
+}
