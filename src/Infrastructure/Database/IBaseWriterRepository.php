5659c147b668ab0740bcd27d4c3ec8f767578aa7
Caio Vinicius Alves Ferreira <caio.ferreira@fretebras.com.br>
2023-05-24 13:01:45 -0300

    refactor: changed interface repository

---
+++ src/Infrastructure/Database/IBaseWriterRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Frete\Core\Infrastructure\Database;
+
+use Frete\Core\Domain\Entity;
+use Frete\Core\Infrastructure\Errors\InfrastructureError;
+
+
+interface IBaseWriterRepository
+{
+    public function upsert(Entity $data): InfrastructureError|bool;
+
+    public function remove(array $filter): InfrastructureError|bool;
+}
