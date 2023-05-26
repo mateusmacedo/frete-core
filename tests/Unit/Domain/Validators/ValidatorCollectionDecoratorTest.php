0c3bb9eaa94474e92dbbfbeb0c989a1c2de7c989
Mateus Macedo Dos Anjos <mateus.anjos@fretebras.com.br>
2023-05-26 04:22:58 -0300

    test: :white_check_mark: validator collection decorator
    - [x] ensure validate decorator works with validator collection

---
+++ tests/Unit/Domain/Validators/ValidatorCollectionDecoratorTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Domain\Validators;
+
+use Frete\Core\Domain\Validators\Validator;
+use Frete\Core\Domain\Validators\ValidatorCollectionDecorator;
+use Tests\TestCase;
+
+final class ValidatorCollectionDecoratorTest extends TestCase
+{
+    private ValidatorCollectionDecorator $sut;
+
+    public function testShouldValidateCollectionWithErrors(): void
+    {
+        $input = ['a', 'b', 'c'];
+        $expected = [
+            'isValid' => false,
+            'errorMessage' => [
+                0 => 'error',
+                1 => 'error',
+                2 => 'error',
+            ]
+        ];
+
+        $validator = $this->createStub(Validator::class);
+        $validator->expects($this->exactly(3))->method('validate')->willReturn(false);
+        $validator->expects($this->exactly(3))->method('getErrorMessage')->willReturn('error');
+
+        $this->sut = new ValidatorCollectionDecorator($validator);
+
+        $actual = [
+            'isValid' => $this->sut->validate($input),
+            'errorMessage' => $this->sut->getErrorMessage()
+        ];
+
+        $this->assertEquals($expected, $actual);
+    }
+}
