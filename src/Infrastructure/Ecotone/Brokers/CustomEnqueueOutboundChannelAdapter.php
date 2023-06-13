7ff2f636a84b3654aa4cf54112489572adf20b81
jefferson.sales <jefferson.sales@fretebras.com.br>
2023-06-13 17:48:39 -0300

    fix: added buildMessage function

--- src/Infrastructure/Ecotone/Brokers/CustomEnqueueOutboundChannelAdapter.php
+++ src/Infrastructure/Ecotone/Brokers/CustomEnqueueOutboundChannelAdapter.php
@@ -8,11 +8,13 @@ use Ecotone\Enqueue\{CachedConnectionFactory, OutboundMessageConverter};
 use Ecotone\Messaging\{Message, MessageHandler, MessageHeaders};
 use Frete\Core\Infrastructure\Ecotone\Brokers\MessageBrokerHeaders\IHeaderMessage;
 use Interop\Queue\Destination;
+use Ecotone\Enqueue\OutboundMessage;
+use Interop\Queue\Message as buildMessageReturn;
 
 abstract class CustomEnqueueOutboundChannelAdapter implements MessageHandler
 {
+    private OutboundMessage $outboundMessage;
     private bool $initialized = false;
-
     public function __construct(
         protected CachedConnectionFactory $connectionFactory,
         protected Destination $destination,
@@ -24,28 +26,27 @@ abstract class CustomEnqueueOutboundChannelAdapter implements MessageHandler
 
     abstract public function initialize(): void;
 
+    protected function buildMessage(Message $message): buildMessageReturn
+    {
+        $this->outboundMessage = $outboundMessage = $this->outboundMessageConverter->prepare($message);
+        $headers = $outboundMessage->getHeaders();
+        $headers[MessageHeaders::CONTENT_TYPE] = $outboundMessage->getContentType();
+
+        $messageBrokerHeaders = $this->messageBrokerHeaders?->getSchema() ?? [];
+
+        return $this->connectionFactory->createContext()->createMessage($outboundMessage->getPayload(), $headers, $messageBrokerHeaders);
+    }
+
     public function handle(Message $message): void
     {
         if ($this->autoDeclare && !$this->initialized) {
             $this->initialize();
             $this->initialized = true;
         }
-
-        $outboundMessage = $this->outboundMessageConverter->prepare($message);
-        $messageBrokerHeaders = $this->messageBrokerHeaders?->getSchema() ?? [];
-
-        $messageToSend = $this->connectionFactory->createContext()->createMessage(
-            $outboundMessage->getPayload(),
-            [
-                MessageHeaders::CONTENT_TYPE => $outboundMessage->getContentType(),
-                $outboundMessage->getHeaders()
-            ],
-            $messageBrokerHeaders
-        );
-
+        $messageToSend = $this->buildMessage($message);
         $this->connectionFactory->getProducer()
-            ->setTimeToLive($outboundMessage->getTimeToLive())
-            ->setDeliveryDelay($outboundMessage->getDeliveryDelay())
+            ->setTimeToLive($this->outboundMessage->getTimeToLive())
+            ->setDeliveryDelay($this->outboundMessage->getDeliveryDelay())
             ->send($this->destination, $messageToSend);
     }
 }
