ec61aa6e0580e2a1b5b6ddc317ebb89a419ca0f7
Mateus Macedo Dos Anjos <mateus.anjos@fretebras.com.br>
2023-06-19 18:58:41 -0300

    fix: :ambulance: return result instance

--- src/Application/Dispatcher.php
+++ src/Application/Dispatcher.php
@@ -3,9 +3,11 @@
 declare(strict_types=1);
 
 namespace Frete\Core\Application;
+
 use Frete\Core\Domain\Event;
+use Frete\Core\Shared\Result;
 
 interface Dispatcher
 {
-    public function dispatch(Action|Event $message): void;
+    public function dispatch(Action|Event $message): Result;
 }
