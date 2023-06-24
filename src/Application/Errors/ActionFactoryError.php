883208eb2ff00a62520d45a61d4689bec224a9f3
Mateus Macedo Dos Anjos <mateus.anjos@fretebras.com.br>
2023-06-24 08:29:19 -0300

    feat: :sparkles: new application errors
    This change introduces new PHP classes for ActionFactoryError.php, DispatcherError.php, and HandlerError.php which inherit from the ApplicationError class, to provide specific exceptions for application errors. Additionally, InvalidDataError.php has been deprecated and should now be replaced with either ValidationError or SpecificationError from the Domain\Errors namespace.

---
+++ src/Application/Errors/ActionFactoryError.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Frete\Core\Application\Errors;
+
+class ActionFactoryError extends ApplicationError
+{
+}
--- src/Application/Errors/InvalidDataError.php
+++ src/Application/Errors/InvalidDataError.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Frete\Core\Application\Errors;
 
+/**
+ * @deprecated version 0.7.0 Use Frete\Core\Domain\Errors\ValidationError or Frete\Core\Domain\Errors\SpecificationError instead
+ */
 class InvalidDataError extends ApplicationError
 {
 }
