9a2b6039fb0dbf36fefe705a940e3352a6343294
Mateus Macedo Dos Anjos <mateus.anjos@fretebras.com.br>
2023-07-06 06:45:25 -0300

    refactor: ♻️ dispatcher and dispatcher bus methods
    - Short description: Refactor dispatch method and add dispatchStore method
    - Detailed description: This commit refactors the `dispatch` method in the `Dispatcher` interface and the `DispatcherBus` class by removing the `messageMetadata` parameter and updating the type checks using the `is_a` function. It also adds a new `dispatchStore` method to the `DispatcherBus` class for dispatching events from an event store.

--- src/Application/EventStoreDispatcher.php
+++ src/Application/EventStoreDispatcher.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace Frete\Core\Application;
 
 use Frete\Core\Domain\EventStore;
+use Frete\Core\Shared\Result;
 
 interface EventStoreDispatcher
 {
-    public function dispatchStore(EventStore $store): void;
+    public function dispatchStore(EventStore $store): Result;
 }
